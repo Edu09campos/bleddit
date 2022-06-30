d8ec6ca79765d19e865fd6569b58c55eed6f6fbd
Eduardo Campos <eduardocampos@MBP-de-Eduardo.lan>
2022-06-30 15:07:47 +0100

    began community page structure and layout

---
+++ src/pages/r/[communityId]/index.tsx
@@ -0,0 +1,54 @@
+import { doc, getDoc } from "firebase/firestore";
+import { GetServerSidePropsContext } from "next";
+import React from "react";
+import { Community } from "../../../atoms/communitiesAtom";
+import { firestore } from "../../../firebase/clientApp";
+import safeJsonStringify from "safe-json-stringify";
+import NotFound from "../../../components/community/notFound";
+import Header from "../../../components/community/header";
+import PageContent from "../../../components/layouts/pageContent";
+
+type Props = {
+  communityData: Community;
+};
+
+const CommunityPage = ({ communityData }: Props) => {
+  if (!communityData) {
+    return <NotFound />;
+  }
+
+  return (
+    <>
+      <Header communityData={communityData} />
+      <PageContent>
+        <></>
+        <></>
+      </PageContent>
+    </>
+  );
+};
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  try {
+    const communityDocRef = doc(
+      firestore,
+      "communities",
+      context.query.communityId as string
+    );
+    const communityDoc = await getDoc(communityDocRef);
+
+    return {
+      props: {
+        communityData: communityDoc.exists()
+          ? JSON.parse(
+              safeJsonStringify({ id: communityDoc.id, ...communityDoc.data() })
+            )
+          : "",
+      },
+    };
+  } catch (error) {
+    console.log("[getServerSideProps] ERROR >>> ", error);
+  }
+}
+
+export default CommunityPage;
---
+++ src/pages/r/[communityId]/submit.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const submit = (props: Props) => {
+  return <div>submit</div>;
+};
+
+export default submit;
